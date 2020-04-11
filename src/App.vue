1f1e70db6060427947b4bd2edac3065426a81444
David Atanda <atandadave@gmail.com>
2020-04-11 21:52:04 +0100

    done with starter file

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <app-header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'app-header': Header
+    }
+  }  
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Baloo+Thambi+2&display=swap');
+  body, html {
+    margin: 0;
+    font-family: 'Baloo Thambi 2', cursive;
+    background-color:lightyellow;
+  }
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,80 @@
+<template>
+  <header id="header">
+    <div class="logo">
+      <router-link to="/">Vue Authenticate</router-link>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/dashboard">Dashboard</router-link>
+        </li>
+        <li >
+          <router-link to="/signup">Register</router-link>
+        </li>
+        <li>
+          <router-link to="/signin">Log In</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+<style scoped>
+
+ * {
+   cursor: pointer;
+ }
+  #header {
+    height: 56px;
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color:#747080;
+    padding: 0 20px;
+  }
+
+  .logo {
+    font-weight: bold;
+    color: white;
+  }
+
+  .logo a {
+    text-decoration: none;
+    color: white;
+  }
+
+  nav {
+    height: 100%;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+  }
+
+  li {
+    margin: 0 16px;
+  }
+
+  li a {
+    text-decoration: none;
+    color: black;
+  }
+
+  li a:hover,
+  li a:active,
+  li a.router-link-active {
+    color: #fa9f;
+  }
+</style>
